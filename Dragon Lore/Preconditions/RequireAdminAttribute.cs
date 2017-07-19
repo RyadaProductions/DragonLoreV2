56cb5e59a6c28dc9d345d20ff8f433f0547a3c30
Hsu Still <341464@gmail.com>
2017-07-19 21:28:21 +0800

    Removed try catch

--- Dragon Lore/Preconditions/RequireAdminAttribute.cs
+++ Dragon Lore/Preconditions/RequireAdminAttribute.cs
@@ -16,25 +16,17 @@ namespace DragonLore.Preconditions
 
         public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
-            try
-            {
-                //@@@ for some reason map does not contain any services so everything i get from it creates a Null exception.
-                // This is probably caused by the Discord.NET library, and should need further investigation.
-                _channels = services.GetRequiredService<IChannels>();
-                _roles = services.GetRequiredService<IRoles>();
+            //@@@ for some reason map does not contain any services so everything i get from it creates a Null exception.
+            // This is probably caused by the Discord.NET library, and should need further investigation.
+            _channels = services.GetRequiredService<IChannels>();
+            _roles = services.GetRequiredService<IRoles>();
 
-                var user = context.Message.Author as SocketGuildUser;
-                if (user == null) return Task.FromResult(PreconditionResult.FromError("This command must be used in a guild."));
+            var user = context.Message.Author as SocketGuildUser;
+            if (user == null) return Task.FromResult(PreconditionResult.FromError("This command must be used in a guild."));
 
-                if (user.Roles.Contains(_roles.Admin))
-                    return Task.FromResult(context.Channel.Id == _channels.AdminChannel ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("You are not in a admin channel"));
-                return Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
-            }
+            if (user.Roles.Contains(_roles.Admin))
+                return Task.FromResult(context.Channel.Id == _channels.AdminChannel ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("You are not in a admin channel"));
+            return Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
         }
     }
 }
