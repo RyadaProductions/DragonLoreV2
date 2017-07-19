74e98e7190c87b477e999c86255defde53f7d84e
Hsu Still <341464@gmail.com>
2017-07-19 21:18:48 +0800

    RequireAdminAttribute cleanup.
    - Removed unncessary keyword 'async'.
    + Added a null check for user.
    * Replaced GetService with GetRequiredService.
    * Cleaned up code formats.

--- Dragon Lore/Preconditions/RequireAdminAttribute.cs
+++ Dragon Lore/Preconditions/RequireAdminAttribute.cs
@@ -1,44 +1,40 @@
-﻿using Discord;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
 using DragonLore.MagicNumbers.Channels;
 using DragonLore.MagicNumbers.Roles;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DragonLore.Preconditions
 {
-  internal class RequireAdminPermission : PreconditionAttribute
-  {
-    private IChannels _channels;
-    private IRoles _roles;
-
-    public async override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
+    internal class RequireAdminPermission : PreconditionAttribute
     {
-      try
-      {
-        //@@@ for some reason map does not contain any services so everything i get from it creates a Null exception.
-        // This is probably caused by the Discord.NET library, and should need further investigation.
-        _channels = services.GetService<IChannels>();
-        _roles = services.GetService<IRoles>();
+        private IChannels _channels;
+        private IRoles _roles;
 
-        var user = context.Message.Author as SocketGuildUser;
-
-        if (user.Roles.Contains(_roles.Admin))
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
-          if (context.Channel.Id == _channels.AdminChannel)
-            return await Task.FromResult(PreconditionResult.FromSuccess());
-          return await Task.FromResult(PreconditionResult.FromError("You are not in a admin channel"));
+            try
+            {
+                //@@@ for some reason map does not contain any services so everything i get from it creates a Null exception.
+                // This is probably caused by the Discord.NET library, and should need further investigation.
+                _channels = services.GetRequiredService<IChannels>();
+                _roles = services.GetRequiredService<IRoles>();
+
+                var user = context.Message.Author as SocketGuildUser;
+                if (user == null) return Task.FromResult(PreconditionResult.FromError("This command must be used in a guild."));
+
+                if (user.Roles.Contains(_roles.Admin))
+                    return Task.FromResult(context.Channel.Id == _channels.AdminChannel ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("You are not in a admin channel"));
+                return Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
+            }
         }
-        return await Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e);
-        return await Task.FromResult(PreconditionResult.FromError("You must be an admin to run this command."));
-      }
     }
-  }
 }
